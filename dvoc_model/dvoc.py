847e0f6935f7cde5555b26f8ad2952b955728ffd
Donny Zimmanck <dzimmanck@enphaseenergy.com>
2021-09-24 15:55:29 -0700

    Used currents for dispatch rather than p and q.  Saves a divide.

--- dvoc_model/dvoc.py
+++ dvoc_model/dvoc.py
@@ -20,8 +20,8 @@ class Dvoc:
         # self.omega_nom = 1 / sqrt(l*c)
         self.omega_nom = TWO_PI * hz_nom
 
-        self.p = 0 
-        self.q = 0
+        self.ia = 0 
+        self.ir = 0
 
         # initialize state variables
         self.v = SQRT_2*80
@@ -37,39 +37,23 @@ class Dvoc:
 
 
     def step(self, dt, i):
-        # power at the terminal
-        v = AlphaBeta.from_polar(self.v, self.theta)
-        p = THREE_HALVES * (v.alpha * i.alpha + v.beta * i.beta)
-        q = THREE_HALVES * (v.beta * i.alpha - v.alpha * i.beta)
+        # currents
+        sin_cos = SinCos.from_theta(self.theta)
+        idq0 = i.to_dq0(sin_cos)
+        ia = idq0.d
+        ir = -idq0.q
 
         # feedback terms
-        p_err = p - self.p
-        q_err = q - self.q
-        v_err = self.sin_phi * q_err + self.cos_phi * p_err
-        theta_err = self.sin_phi * p_err - self.cos_phi * q_err
-
+        ia_err = ia - self.ia
+        ir_err = ir - self.ir
+        v_err = self.sin_phi * ir_err + self.cos_phi * ia_err
+        theta_err = self.sin_phi * ia_err - self.cos_phi * ir_err
 
         dvdt = self.k0 * self.v * (self.two_v_nom_srd - 2*self.v**2) \
-               - self.k1 * v_err / self.v
+               - self.k1 * v_err
 
         dthetadt = self.omega_nom \
-               - self.k1 * theta_err / (self.v**2)
-
-
-        # %terminal voltage in alpha/beta frame
-        # v_a = sqrt(2)*x(1)*cos(x(2));
-        # v_b = sqrt(2)*x(1)*sin(x(2));
-
-        # %power at the terminal
-        # P = 3/2*(v_a*x(3) + v_b*x(4));
-        # Q = 3/2*(v_b*x(3) - v_a*x(4));
-
-        # %grid voltage
-        # vg_a = sqrt(2)*vg_mag*cos(omega_nom*t);
-        # vg_b = sqrt(2)*vg_mag*sin(omega_nom*t);
-
-        # dx(1) = eps/k_v^2*x(1)*(2*V_nom^2 - 2*x(1)^2) - k_v*k_i/(3*C*x(1))*(sin(varphi)*(Q - Q_ref) + cos(varphi)*(P - P_ref)); %V
-        # dx(2) = omega_nom - k_v*k_i/(3*C*x(1)^2)*(sin(varphi)*(P - P_ref) - cos(varphi)*(Q - Q_ref)); %theta
+               - self.k1 * theta_err / self.v
 
         self.v += dvdt * dt
         self.theta += dthetadt * dt
@@ -86,7 +70,7 @@ if __name__ == "__main__":
     Rf = 0.4
 
     # simulation parameters
-    dt = 1 / 10e3
+    dt = 1 / 100e3
     ts = np.arange(0, 500e-3, dt)
 
     # grid parameters
@@ -94,8 +78,8 @@ if __name__ == "__main__":
     grid_omega = TWO_PI * 60
 
     # create a step function for dispatch (3A to 6A)
-    ps = 250 * np.ones(len(ts))
-    ps[len(ts)//2:] = 500
+    ias = 3 * np.ones(len(ts))
+    ias[len(ts)//2:] = 6
 
     # create an oscillator using the defaults
     dvoc = Dvoc()
@@ -110,9 +94,9 @@ if __name__ == "__main__":
 
     # run simulation
     i = AlphaBeta(0, 0, 0)  # start out current at 0A
-    for p, t in zip(ps, ts):
+    for ia, t in zip(ias, ts):
         # update dispatch
-        dvoc.p = p
+        dvoc.ia = ia
 
         # update the grid voltage
         sin_cos = SinCos.from_theta(grid_omega * t)
